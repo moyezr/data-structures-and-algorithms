5a12ae6370c259ebc0e807803eedf8859f03a92e
Moyez <moyezrabbani@gmail.com>
2023-11-09 23:32:36 +0530

    Learned quick sort

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,21 @@
+package BinaryTree;
+
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public String toString()   {
+        return "Value = " + data;
+    }
+}
+
+public class BinaryTree {
+
+
+}
--- Sorting/BubbleSortRec.java
+++ Sorting/BubbleSortRec.java
@@ -4,25 +4,29 @@ import java.sql.SQLOutput;
 
 public class BubbleSortRec {
 
-    static int[] sortRec(int arr[], int index) {
+    static void sortRec(int arr[], int r, int c) {
 
-        if(index == arr.length - 1) {
-            return arr;
+        if (r == 0) {
+            return;
         }
 
-        if(arr[index] > arr[index + 1]){
-            int temp = arr[index];
-            arr[index] = arr[index + 1];
-            arr[index + 1] = temp;
+        if (c < r) {
+            if (arr[c] > arr[c + 1]) {
+//              swap
+                int temp = arr[c];
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
+            }
+            sortRec(arr, r, c + 1);
+        } else {
+            sortRec(arr, r - 1, 0);
         }
-
-        return sortRec(arr, index + 1);
 
     }
 
     static void print(int arr[]) {
         System.out.print("[");
-        for(int num: arr) {
+        for (int num : arr) {
             System.out.print(num + ", ");
         }
         System.out.print("\b\b]");
@@ -30,8 +34,9 @@ public class BubbleSortRec {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 3, 2, 5, 8};
-        sortRec(arr, 0);
+        int arr[] = {1, 3, 2, 5, 8, -4, -2, 10};
+        print(arr);
+        sortRec(arr, arr.length - 1, 0);
         print(arr);
     }
 }
