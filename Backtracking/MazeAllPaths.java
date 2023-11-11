1ffec621495f3a26405856eb97f6ae0aa168af51
Moyez <moyezrabbani@gmail.com>
2023-11-11 15:40:21 +0530

    Added a function that prints the maze with the paths followed in number of steps

--- Backtracking/MazeAllPaths.java
+++ Backtracking/MazeAllPaths.java
@@ -1,5 +1,8 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MazeAllPaths {
 
     public static void main(String[] args) {
@@ -11,13 +14,58 @@ public class MazeAllPaths {
                 {true, true, true},
         };
 
-        allPathWithRestrictions("", maze, 0, 0);
+        int arr[][] = new int[3][3];
+
+//        allPath("", maze, 0, 0);
+
+        allPathPrint("", maze, 0, 0, arr, 0);
 
     }
 
-    static void allPathWithRestrictions(String p, boolean[][] maze, int r, int c) {
+    static void allPath(String p, boolean[][] maze, int r, int c) {
         if (r == maze.length - 1 && c == maze[0].length - 1) {
             System.out.print(p + " ");
+            return;
+        }
+
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // I am considering this block in my path.
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            allPath(p + "D", maze, r + 1, c);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPath(p + "R", maze, r, c + 1);
+        }
+
+        if (r > 0) {
+            allPath(p + "U", maze, r - 1, c);
+        }
+
+        if (c > 0) {
+            allPath(p + "L", maze, r, c - 1);
+        }
+
+        // this line is where the function will be over;
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+    }
+
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for(int[] arr: path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println("\n\n");
+
             return;
         }
 
@@ -28,25 +76,28 @@ public class MazeAllPaths {
         // I am considering this block in my path.
         maze[r][c] = false;
 
+        path[r][c] = step;
+
         if (r < maze.length - 1) {
-            allPathWithRestrictions(p + "D", maze, r + 1, c);
+            allPathPrint(p + "D", maze, r + 1, c, path, step + 1);
         }
 
         if (c < maze[0].length - 1) {
-            allPathWithRestrictions(p + "R", maze, r, c + 1);
+            allPathPrint(p + "R", maze, r, c + 1, path, step + 1);
         }
 
         if (r > 0) {
-            allPathWithRestrictions(p + "U", maze, r - 1, c);
+            allPathPrint(p + "U", maze, r - 1, c, path, step + 1);
         }
 
         if (c > 0) {
-            allPathWithRestrictions(p + "L", maze, r, c - 1);
+            allPathPrint(p + "L", maze, r, c - 1, path, step + 1);
         }
 
         // this line is where the function will be over;
         // so before the function gets removed, also remove the changes that were made by that function
         maze[r][c] = true;
+        path[r][c] =0;
     }
 
 }
