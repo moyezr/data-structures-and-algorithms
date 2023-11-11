64759fbc9e62af4f16af1d0de0604c77b5c61d51
Moyez <moyezrabbani@gmail.com>
2023-11-11 14:55:38 +0530

    Added function to print the path

--- Backtracking/Maze.java
+++ Backtracking/Maze.java
@@ -1,9 +1,19 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Maze {
 
     public static void main(String[] args) {
-        System.out.println(count(3, 3));
+//        System.out.println(count(3, 3));
+        ArrayList<String> arr = new ArrayList<>();
+
+
+        path(3, 3, "", arr);
+
+        System.out.println(arr);
+
 
     }
 
@@ -19,6 +29,22 @@ public class Maze {
         return left + right;
     }
 
+    static void path(int r, int c, String ans, ArrayList<String> arr) {
+        if (r == 1 && c == 1) {
+            arr.add(ans);
+            System.out.print(ans + "  ");
+            return;
+        }
+
+        if (r > 1) {
+            path(r - 1, c, ans + "D", arr);
+        }
+        if (c > 1) {
+            path(r, c - 1, ans + "R", arr);
+        }
+
+
+    }
 
 
 }
