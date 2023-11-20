3cbb9b1c57a7d0ebfdd4e773549e57fc44abc4a9
Moyez <moyezrabbani@gmail.com>
2023-11-20 12:55:15 +0530

    added toString functions

--- Graphs/UndirectedWeightedGraph.java
+++ Graphs/UndirectedWeightedGraph.java
@@ -10,23 +10,34 @@ public class UndirectedWeightedGraph {
     private class Node {
         private String label;
 
-        private List<Graphs.WeightedEdge> edges;
+        private List<WeightedEdge> edges;
 
         public Node(String label) {
             this.label = label;
-            this.edges = new ArrayList<>();
+            this.edges = new ArrayList<WeightedEdge>();
         }
 
-        public 
+        public void addEdge(Node to, int weight) {
+            this.edges.add(new WeightedEdge(to, weight));
+            to.edges.add(new WeightedEdge(this, weight));
+        }
+
+        public String toString() {
+            return this.label;
+        }
     }
 
     private class WeightedEdge {
-        public Graphs.Node to;
+        public Node to;
         public int weight;
 
-        public WeightedEdge(Graphs.Node to, int weight) {
+        public WeightedEdge(Node to, int weight) {
             this.weight = weight;
             this.to = to;
+        }
+
+        public String toString() {
+            return "{ Node= " + to + ", Weight=" + this.weight + "}";
         }
     }
 
